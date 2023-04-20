e3677daad1592268fa1837d601b4e2875e021c9e
Kitari31 <76065071+Kitari31@users.noreply.github.com>
2023-04-20 17:26:30 +0200

    Exo2

--- src/welcome/Welcome.java
+++ src/welcome/Welcome.java
@@ -3,6 +3,10 @@ package welcome;
 public class Welcome {
 
 	public static String welcome(String input) {
+		if(input==null)
+			return "Hello, my friend";
+		if(input.trim().isEmpty())
+			return "Hello, my friend";
 		return "Hello, " + input.substring(0,1).toUpperCase() + input.substring(1);
 	}
 }
